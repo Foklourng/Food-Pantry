3043a16ad952755a074a8fe6afb7bb66d93dab2f
Foklourng <58053766+Foklourng@users.noreply.github.com>
2020-04-15 03:20:03 -0700

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,23 @@
+let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
+                //let url = "https://spreadsheets.google.com/feeds/list/"+spreadsheetID+"/od6/public/values?alt=json";
+                let url ="https://spreadsheets.google.com/feeds/list/"+spreadsheetID+"/od6/public/values?alt=json";
+                jQuery.getJSON(url, function(data){
+                let entry = data.feed.entry;
+                    jQuery(entry).each(function(){
+                        let pic = this.gsx$picture.$t;
+                        let name = this.gsx$name.$t;
+                        let stat = this.gsx$status.$t;
+                        let lim = this.gsx$limit.$t;
+                        jQuery("#t-body").append(`
+                            <tr>
+                                <td><img src=${pic} alt="Laptop" height="200" width="200" ></td>
+                              
+                                <td>${name}</td>
+                                <td>${stat}</td>
+                                <td>${lim}</td>
+                            </tr>
+                        `);
+                    });
+                    
+                });
+            
