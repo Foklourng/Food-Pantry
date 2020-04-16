5348a90fd593eb5110f3ab2cec43b0ae34c038c2
Foklourng <58053766+Foklourng@users.noreply.github.com>
2020-04-16 16:51:46 -0700

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -8,6 +8,8 @@ let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
                         let name = this.gsx$name.$t;
                         let stat = this.gsx$status.$t;
                         let lim = this.gsx$limit.$t;
+                        let type = this.gsx$type.$t;
+
                         jQuery("#t-body").append(`
                             <tr>
                                 <td><img src=${pic} alt="Laptop" height="200" width="200" ></td>
@@ -17,7 +19,33 @@ let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
                                 <td>${lim}</td>
                             </tr>
                         `);
+
                     });
                     
                 });
+
+                function filter(t){
+                    $("#t-body").empty();
+
+                    jQuery.getJSON(url, function(data){
+                let entry = data.feed.entry;
+                    jQuery(entry).each(function(){
+                        let type = this.gsx$type.$t;
+                        if(type == t)
+                        {
+                        jQuery("#t-body").append(`
+                            <tr>
+                                <td><img src=${pic} alt="Laptop" height="200" width="200" ></td>
+                              
+                                <td>${name}</td>
+                                <td>${stat}</td>
+                                <td>${lim}</td>
+                            </tr>
+                        `);
+                        }
+                    });
+                
+                    
+                });
+                }
             
