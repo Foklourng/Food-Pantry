01cc990eb6e537e824bc413ac3f7b9e72ec252e6
Foklourng <58053766+Foklourng@users.noreply.github.com>
2020-05-06 15:20:29 -0700

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,8 +1,11 @@
 let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
                 //let url = "https://spreadsheets.google.com/feeds/list/"+spreadsheetID+"/od6/public/values?alt=json";
                 let url ="https://spreadsheets.google.com/feeds/list/"+spreadsheetID+"/od6/public/values?alt=json";
-                jQuery.getJSON(url, function(data){
+				
+				jQuery.getJSON(url, function(data){
                 let entry = data.feed.entry;
+				var c =2, itemN;
+				
                     jQuery(entry).each(function(){
                         let pic = this.gsx$picture.$t;
                         let name = this.gsx$name.$t;
@@ -12,18 +15,51 @@ let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
 
                         jQuery("#t-body").append(`
                             <tr>
-                                <td><img src=${pic} alt="Laptop" height="200" width="200" ></td>       
+                                <td><img src="Images//${pic}" alt="Laptop" height="100" width="100" ></td>                              
                                 <td>${name}</td>
                                 <td>${stat}</td>
                                 <td>${lim}</td>
+								<td><button class="btn" class="editbtn"value='${c}' onclick="setIN(value)" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-edit"  "></i></button>
+								<button class="btn" class="editbtn"value='${c}' onclick="remove(value)"  data-target="#exampleModalCenter"><i class="fa fa-trash"  "></i></button></td>
                             </tr>
                         `);
-
-                    });
-                    
+                        c++;
+					});
+                    lastNum =c;
                 });
+				var lastNum;
+		function sub(){
+				var image = document.getElementById('myImg');
+				var imgIn = document.getElementById('sub');
+				image.style.display = "block";
+				var file = imgIn.value.split("\\");
+				var fileName = file[file.length-1];
+				image.src = fileName;
+				console.log('name' + document.getElementById('firstN').value);
+				console.log('status' + document.getElementById('status').value);
+				console.log('limit' + document.getElementById('limit').value);
+				console.log('Image' + fileName);
+		}
+		function imageIsLoaded() { 
+			alert(this.src);  // blob url
+			// update width and height ...
+		  }
 
-        
+         function setIN(c){
+			itemN =c;
+		  }
+		  function returnIN(){
+				return itemN;
+		  }
+		 
+		  function returnLN(){
+			  console.log("Last Number is " + lastNum);
+			return lastNum;
+	  }
+		  function remove(c){
+			  console.log("Removing");
+		  }
+
 		function filter(t){
 			console.log("Hello");
                     $("#t-body").empty();
@@ -37,7 +73,7 @@ let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
                         let lim = this.gsx$limit.$t;
                         let type = this.gsx$type.$t;
 						if(t== "1"){
-							if(type == "Can")
+							if(type == "canned")
 							{
 							jQuery("#t-body").append(`
 								<tr>
@@ -49,7 +85,7 @@ let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
 							`);
 							}
 						}else if(t== "2"){
-							if(type == "Snack")
+							if(type == "cereal")
 							{
 							jQuery("#t-body").append(`
 								<tr>
@@ -61,7 +97,7 @@ let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
 							`);
 							}
 						}else if(t== "3"){
-							if(type == "Drink")
+							if(type == "Refrigerator")
 							{
 							jQuery("#t-body").append(`
 								<tr>
@@ -73,7 +109,7 @@ let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
 							`);
 							}
 						}else if(t== "4"){
-							if(type == "Noodle")
+							if(type == "Personal/Toiletries")
 							{
 							jQuery("#t-body").append(`
 								<tr>
@@ -86,6 +122,19 @@ let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
 							}
 						}
 						else if(t== "5"){
+							if(type == "Other")
+							{
+							jQuery("#t-body").append(`
+								<tr>
+									<td><img src=${pic} alt="Laptop" height="200" width="200" ></td>                              
+									<td>${name}</td>
+									<td>${stat}</td>
+									<td>${lim}</td>
+								</tr>
+							`);
+							}
+						}
+						else if(t== "6"){
 							jQuery("#t-body").append(`
 								<tr>
 									<td><img src=${pic} alt="Laptop" height="200" width="200" ></td>                              
@@ -96,14 +145,23 @@ let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
 							`);
 							
 						}
-
                     });
                 
                     
                 });
                 }
 				
-			            
+	function submit(){
+		var a = document.getElementById("firstN").value;
+		var b = document.getElementById("status").value;
+		var c = document.getElementById("limit").value;
+		var d = document.getElementById("firstN").value;
+		console.log(a);
+		console.log(b);
+		console.log(c);
+		console.log(d);
+
+	}     
 			
 	$(document).ready(function(){
   $("#myInput").on("keyup", function() {
@@ -113,4 +171,5 @@ let spreadsheetID = "1849CSeI6TmOPOeuwDMdPDa5f-xDNT6aCOr1OZB96kwQ"; // Sheet ID
     });
   });
 });
+
 
